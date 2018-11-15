dc32757290a4115e97073733bee7ad6be954b9eb
박기성 <bleuflares@gmail.com>
2018-11-15 15:25:57 +0900

    used join instead of cartesian but does not work...

--- hw3_2.py
+++ hw3_2.py
@@ -21,18 +21,20 @@ def matmul(row):
 def parser(line):
     points = line.split()
     if len(points) > 1:
-        return (points[0], points[1])
+        return (int(points[0]), int(points[1]))
 
 def normalize(element):
     return (element[0], element[1][0], 1.0 / float(element[1][1]))
 
+def mapper(element):
+    return (element[1], (element[0], element[2]))
 def teleport(pair):
-    return (pair[0], pair[1][0] + 0.3 * 0.001)
+    return (pair[0], pair[1] + 0.3 * 0.001)
 
 if __name__ == "__main__":
 
     BETA = 0.9
-    arr = [(i, 0, 0.001) for i in range(1000)]
+    arr = [(0, i, 0.001) for i in range(1000)]
 
     conf = SparkConf()
     sc = SparkContext(conf=conf)
@@ -42,15 +44,31 @@ if __name__ == "__main__":
     col_count = sc.parallelize(edges.countByKey())
     mat = edges.join(col_count)
     norm_mat = mat.flatMap(normalize)
+    norm_mat_pair = norm_mat.map(mapper)
     v = sc.parallelize(arr)
     v_old = v
+    v_old_pair = v_old.map(mapper)
 
     for j in range(50):
-        matmuls = norm_mat.cartesian(v_old).filter(lambda x: x[0][1] == x[1][0])
-        matmul = matmuls.map(lambda x: (x[0][0], BETA * x[0][2] * x[1][2])).reduceByKey(lambda a, b: a + b)
+        #matmuls = norm_mat.cartesian(v_old).filter(lambda x: x[0][1] == x[1][0])
+
+        norm_mat_pair.join(v_old_pair)
+
+        matmul = norm_mat_pair.map(lambda x: (x[1][0][0], BETA * x[1][0][1] * x[1][1][1])).reduceByKey(lambda a, b: a + b)
+        print(matmul.collect())
         v_new = matmul.map(teleport)
         v_old = v_new
 
-    print(sorted(v_old.collect())[-10:])
+"""
+    vec = sc.parallelize(arr)
+    v = vec.flatMap(form_changer)
+    v_old = v
 
-
+    for j in range(50):
+        mulpairs = norm_mat.join(v_old)
+        matmul = mulpairs.map(lambda x: (x[0], BETA * x[1][0][1] * x[1][1][1])).reduceByKey(lambda a, b: a + b)
+        v_new = matmul.map(teleport)
+        v_old = v_new
+        print(v_old.collect())
+    
+"""
